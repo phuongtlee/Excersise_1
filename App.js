b41a2da57ed22a38237c68cc1c9967f441c88b6a
phuongtlee <97996632+phuongtlee@users.noreply.github.com>
2024-04-05 13:47:26 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,54 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView, Text, View } from "react-native";
-import SectionListTutorial from "./SelectionListTutorial";
-import ReadDataFunc from "./ReadDataFunc";
-import PictureTutorial from "./PictureTutorial";
-import JustifyContentTutorial from "./JustifyContentTutorial";
-import HeightWidthTutorial from "./HeightWidthTutorial";
-import FlexTutorial from "./FlexTutorial";
-import FlexDirectionTutorial from "./FlexDirectionTutorial";
-import FlatListTutorial from "./FlatListTutorial";
-import ClassTutorial from "./ClassTutorial";
-import ButtonTutorial from "./ButtonTutorial";
-import PropsTutorial from "./PropsTutorial";
-import PropFatherTutorial from "./PropFatherTutorial";
-import StateTutorial from "./StateTutorial";
-import StateToInputTutorial from "./StateToInputTutorial";
-import SendDataTutorial from "./SendDataTutorial";
-import SendDataFromEditText2 from "./SendDataFromEditText";
-import Cat from "./Cat";
-import Cafe from "./Cat";
-import App1 from "./App1";
-import InputTextExam from "./InputTextExam";
 import Excersise1 from "./Excersise1";
 // import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      {/* <SectionListTutorial/> */}
-      {/* <ReadDataFunc/> */}
-      {/* <PictureTutorial/> */}
-      {/* <JustifyContentTutorial/> */}
-      {/* <HeightWidthTutorial/> */}
-      {/* <FlexTutorial/> */}
-      {/* <FlexDirectionTutorial/> */}
-      {/* <FlatListTutorial/> */}
-      {/* <ClassTutorial/> */}
-      {/* <ButtonTutorial/> */}
-      {/* <PropsTutorial tenNguoi="Nguyen Van A"/> */}
-      {/* <PropFatherTutorial />
-      <PropsTutorial ten='Nguyen Van V'/> */}
-      {/* <StateTutorial/> */}
-      {/* <StateToInputTutorial/> */}
-      {/* <SendDataTutorial/> */}
-      {/* <SendDataFromEditText2/> */}
-      {/* <Cafe /> */}
-      {/* <App1/> */}
-      {/* <InputTextExam/> */}
+    <View style={styles.container}>    
       <Excersise1/>
-
     </View>
   );
 }
